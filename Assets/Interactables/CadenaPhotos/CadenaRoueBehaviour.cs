bfe8f6cf2b8988801c8109a899265469b487ac33
Nicolas Cortial <75365622+IreshDeragon@users.noreply.github.com>
2023-07-12 15:47:57 +0200

    correction roue

--- Assets/Interactables/CadenaPhotos/CadenaRoueBehaviour.cs
+++ Assets/Interactables/CadenaPhotos/CadenaRoueBehaviour.cs
@@ -7,22 +7,23 @@ public class CadenaRoueBehaviour : MonoBehaviour
     public int value;
     public float rotationSpeed;
     Vector3 direction = new Vector3(0f, 0f, 1f);
+    Transform child;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        child = transform.GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.rotation.eulerAngles.z < (value)*40 -0.5 || transform.rotation.eulerAngles.z > (value) * 40 + 0.5)
+        if (child.rotation.eulerAngles.z < (value)*40 -0.5 || child.rotation.eulerAngles.z > (value) * 40 + 0.5)
         {
-            transform.Rotate(direction * rotationSpeed * Time.deltaTime);
+            child.Rotate(direction * rotationSpeed * Time.deltaTime);
         }
     }
-
+    
     public void incrementValue()
     {
         value = (value + 1)%9;
