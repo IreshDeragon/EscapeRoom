571fda258a2d43a560a887b3d939edc7e321974a
Nicolas Cortial <75365622+IreshDeragon@users.noreply.github.com>
2023-07-13 13:35:50 +0200

    ouverture porte (à tester)

--- Assets/Interactables/CadenaPhotos/CadenaBehaviour.cs
+++ Assets/Interactables/CadenaPhotos/CadenaBehaviour.cs
@@ -8,6 +8,9 @@ public class CadenaBehaviour : MonoBehaviour
     public int secondChiffre;
     public int troisiemeChiffre;
     public int[] Solution= new int[3];
+    public GameObject porte;
+    public GameObject handGrab;
+    public GameObject handGrabMirror;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,12 @@ public class CadenaBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(premierChiffre == Solution[0] && secondChiffre == Solution[1] && troisiemeChiffre == Solution[2])
+        {
+            porte.transform.GetComponent<Oculus.Interaction.Grabbable>().enabled = true;
+            handGrab.SetActive(true);
+            handGrabMirror.SetActive(true);
+        }
     }
     public void setCliqued(int clic)
     {
