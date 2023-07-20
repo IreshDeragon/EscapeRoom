a95281a6f4688a8c718c71df5c36982db49b655f
Nicolas Cortial <75365622+IreshDeragon@users.noreply.github.com>
2023-07-20 16:36:43 +0200

    hmm

--- Assets/Interactables/Rupture/HammerBehaviour.cs
+++ Assets/Interactables/Rupture/HammerBehaviour.cs
@@ -7,10 +7,11 @@ public class HammerBehaviour : MonoBehaviour
     public GameObject StrikePoint;
     public Collider colider;
     public bool grabbed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
