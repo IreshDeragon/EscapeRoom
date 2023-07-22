9fe899ef8cd383e8289bbd63071420209d8e8caf
Nicolas Cortial <75365622+IreshDeragon@users.noreply.github.com>
2023-07-22 14:15:36 +0200

    fmod fonctionne

--- Assets/Interactables/Rupture/BrokentHeartBehaviour.cs
+++ Assets/Interactables/Rupture/BrokentHeartBehaviour.cs
@@ -6,15 +6,23 @@ using UnityEngine.Playables;
 public class BrokentHeartBehaviour : MonoBehaviour
 {
     public PlayableDirector director;
+    double time;
+    double timer;
     // Start is called before the first frame update
     void Start()
     {
         director.Play();
+        time = director.duration;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= time)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Interactables/Rupture/HammerBehaviour.cs
+++ Assets/Interactables/Rupture/HammerBehaviour.cs
@@ -8,10 +8,12 @@ public class HammerBehaviour : MonoBehaviour
     public Collider colider;
     public bool grabbed;
 
+    private FMOD.Studio.EventInstance pickEvent;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        pickEvent = FMODUnity.RuntimeManager.CreateInstance("event:/InGame/PuzzleStoneHeart/IG_PSH_take_hammer");
     }
 
     // Update is called once per frame
@@ -22,5 +24,11 @@ public class HammerBehaviour : MonoBehaviour
     public void setGrabbed(bool grab)
     {
         grabbed = grab;
+    }
+
+    public void pickSound()
+    {
+        pickEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        pickEvent.start();
     }
 }
